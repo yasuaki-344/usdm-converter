cea538b7f288e998e29bea1e1f93b31e5bcc3376
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-02-24 21:07:38 +0900

    Create Specification.cs

---
+++ src/ApplicationCore/Entities/Specification.cs
@@ -0,0 +1,12 @@
+﻿namespace UsdmConverter.ApplicationCore.Entities
+{
+    /// <summary>
+    /// Specification entity
+    /// </summary>
+    public class Specification
+    {
+        public bool IsImplemented { get; set; }
+        public string ID { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
