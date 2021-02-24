1b219f1e78fde8e5172005fdcdec64586ede0530
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-02-24 21:12:30 +0900

    Create IMarkdownParser.cs

---
+++ src/ApplicationCore/Interfaces/IMarkdownParser.cs
@@ -0,0 +1,9 @@
+﻿using UsdmConverter.ApplicationCore.Entities;
+
+namespace UsdmConverter.ApplicationCore.Interfaces
+{
+    public interface IMarkdownParser
+    {
+        RequirementSpecification Parse(string content);
+    }
+}
