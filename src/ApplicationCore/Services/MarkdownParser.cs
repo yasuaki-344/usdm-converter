60c5dcac0ce4dba3b58962596e2f212b3a79f42d
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-02-24 21:13:17 +0900

    Create MarkdownParser.cs

---
+++ src/ApplicationCore/Services/MarkdownParser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Toolkit.Parsers.Markdown;
+using Microsoft.Toolkit.Parsers.Markdown.Blocks;
+using UsdmConverter.ApplicationCore.Entities;
+using UsdmConverter.ApplicationCore.Exceptions;
+using UsdmConverter.ApplicationCore.Interfaces;
+
+namespace UsdmConverter.ApplicationCore.Services
+{
+    public class MarkdownParser : IMarkdownParser
+    {
+        private readonly MarkdownDocument _document = new MarkdownDocument();
+
+        /// <summary>
+        /// Initializes a new instance of MarkdownParser class.
+        /// </summary>
+        public MarkdownParser()
+        {
+        }
+
+        /// <summary>
+        /// Parse a markdown document
+        /// </summary>
+        /// <param name="content">The markdown text</param>
+        /// <returns></returns>
+        public RequirementSpecification Parse(string content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
