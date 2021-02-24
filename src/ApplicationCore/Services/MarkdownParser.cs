d5618c888e83a3287c9d465f53dfa1a50d212ba6
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-02-24 23:58:39 +0900

    Update MarkdownParser.cs

--- src/ApplicationCore/Services/MarkdownParser.cs
+++ src/ApplicationCore/Services/MarkdownParser.cs
@@ -26,7 +26,87 @@ namespace UsdmConverter.ApplicationCore.Services
         /// <returns></returns>
         public RequirementSpecification Parse(string content)
         {
-            throw new NotImplementedException();
+            _document.Parse(content);
+
+            var data = new RequirementSpecification();
+            foreach (var element in _document.Blocks)
+            {
+                switch (element.Type)
+                {
+                    case MarkdownBlockType.Code:
+                        Console.WriteLine($"Type: {element.Type}");
+                        break;
+                    case MarkdownBlockType.Header:
+                        var header = element as HeaderBlock;
+                        if (header != null)
+                        {
+                            var headerLevel = header.HeaderLevel;
+                            var text = header.ToString().Trim();
+                            // var count = header.Inlines.Count;
+                            switch (headerLevel)
+                            {
+                                case 1:
+                                    data.Title = text;
+                                    break;
+                                case 2:
+                                    // TODO: IDとの分離
+                                    data.Requirements.Add(
+                                        new UpperRequirement
+                                        {
+                                            Summay = text
+                                        }
+                                    );
+                                    break;
+                                case 3:
+                                    if (text.Equals("理由"))
+                                    {
+                                        data.Requirements.Last().Reason = text;
+                                    }
+                                    else if (text.Equals("説明"))
+                                    {
+                                        data.Requirements.Last().Description = text;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine($"Text: {text}");
+                                    }
+                                    break;
+                                default:
+                                    Console.WriteLine($"Text: {text}");
+                                    break;
+                            }
+                        }
+                        break;
+                    case MarkdownBlockType.HorizontalRule:
+                        Console.WriteLine($"Type: {element.Type}");
+                        break;
+                    case MarkdownBlockType.LinkReference:
+                        Console.WriteLine($"Type: {element.Type}");
+                        break;
+                    case MarkdownBlockType.List:
+                        Console.WriteLine($"Type: {element.Type}");
+                        break;
+                    case MarkdownBlockType.ListItemBuilder:
+                        Console.WriteLine($"Type: {element.Type}");
+                        break;
+                    case MarkdownBlockType.Paragraph:
+                        Console.WriteLine($"Type: {element.Type}");
+                        break;
+                    case MarkdownBlockType.Quote:
+                        Console.WriteLine($"Type: {element.Type}");
+                        break;
+                    case MarkdownBlockType.Root:
+                        Console.WriteLine($"Type: {element.Type}");
+                        break;
+                    case MarkdownBlockType.Table:
+                        Console.WriteLine($"Type: {element.Type}");
+                        break;
+                    default:
+                        Console.WriteLine($"Type: {element.Type}");
+                        break;
+                }
+            }
+            return data;
         }
     }
 }
