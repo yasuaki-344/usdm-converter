6a48fb47dd0cbf3f11a6884dea4a08ead05b88d5
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-02-24 21:08:06 +0900

    Create SpecificationGroup.cs

---
+++ src/ApplicationCore/Entities/SpecificationGroup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace UsdmConverter.ApplicationCore.Entities
+{
+    /// <summary>
+    /// Specification group entity
+    /// </summary>
+    public class SpecificationGroup
+    {
+        public string Category { get; set; } = string.Empty;
+        public List<Specification> Specifications { get; set; } = new List<Specification>();
+    }
+}
