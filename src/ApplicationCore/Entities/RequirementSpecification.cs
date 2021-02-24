03c96dea48195a856fec5293ac1691e290a96cf0
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-02-24 21:11:13 +0900

    Create RequirementSpecification.cs

---
+++ src/ApplicationCore/Entities/RequirementSpecification.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace UsdmConverter.ApplicationCore.Entities
+{
+    /// <summary>
+    /// Requirement specification entity
+    /// </summary>
+    public class RequirementSpecification
+    {
+        public string Title { get; set; } = string.Empty;
+        public List<UpperRequirement> Requirements { get; set; } = new List<UpperRequirement>();
+    }
+}
