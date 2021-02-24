398e108e8a268aa3c72fc1884588fd79ce1587ef
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-02-24 21:11:46 +0900

    Create UsdmMarkDownFormatException.cs

---
+++ src/ApplicationCore/Exceptions/UsdmMarkDownFormatException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UsdmConverter.ApplicationCore.Exceptions
+{
+    public class UsdmMarkDownFormatException : Exception
+    {
+        public UsdmMarkDownFormatException()
+            : base()
+        {
+        }
+
+        public UsdmMarkDownFormatException(string message)
+            : base(message)
+        {
+        }
+    }
+}
