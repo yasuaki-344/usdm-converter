60728d413d5302938032d01526936d636fe5643a
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-02-24 21:23:59 +0900

    Create ExcelWriter.cs

---
+++ src/ApplicationCore/Services/ExcelWriter.cs
@@ -0,0 +1,14 @@
+using UsdmConverter.ApplicationCore.Interfaces;
+
+namespace UsdmConverter.ApplicationCore.Services
+{
+    public class ExcelWriter : IExcelWriter
+    {
+        /// <summary>
+        /// Initializes a new instance of ExcelWriter class.
+        /// </summary>
+        public ExcelWriter()
+        {
+        }
+    }
+}
