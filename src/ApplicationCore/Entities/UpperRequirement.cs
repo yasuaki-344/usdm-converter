0578fcba8637e5bcb9d21c09eb8b59f468b8582e
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-02-24 21:10:30 +0900

    Create UpperRequirement.cs

---
+++ src/ApplicationCore/Entities/UpperRequirement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace UsdmConverter.ApplicationCore.Entities
+{
+    /// <summary>
+    /// Upper requirement entity
+    /// </summary>
+    public class UpperRequirement
+    {
+        public string ID { get; set; } = string.Empty;
+        public string Summay { get; set; } = string.Empty;
+        public string Reason { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<LowerRequirement> Requirements { get; set; } = new List<LowerRequirement>();
+    }
+}
