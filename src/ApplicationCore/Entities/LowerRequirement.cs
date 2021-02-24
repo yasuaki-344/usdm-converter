349430cd0c3e11a416e7ad0863d4937be16f8835
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-02-24 21:09:45 +0900

    Create LowerRequirement.cs

---
+++ src/ApplicationCore/Entities/LowerRequirement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace UsdmConverter.ApplicationCore.Entities
+{
+    /// <summary>
+    /// Lower requirement entity
+    /// </summary>
+    public class LowerRequirement
+    {
+        public string ID { get; set; } = string.Empty;
+        public string Summay { get; set; } = string.Empty;
+        public string Reason { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<SpecificationGroup> SpecificationGroups { get; set; } = new List<SpecificationGroup>();
+    }
+}
