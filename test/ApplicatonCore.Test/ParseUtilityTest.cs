f0f6e04094ef5b4297f1126a3f6f9478df00dd30
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-02-25 22:07:07 +0900

    Create ParseUtilityTest.cs

---
+++ test/ApplicatonCore.Test/ParseUtilityTest.cs
@@ -0,0 +1,21 @@
+﻿using UsdmConverter.ApplicationCore.Logic;
+using Xunit;
+
+namespace UsdmConverter.ApplicatonCore.Test
+{
+    public class ParseUtilityTest
+    {
+        [Fact]
+        public void DecomposeHeadingCorrectly()
+        {
+            var expectId = "REQ01";
+            var expectSummary = "requirement";
+            var input = $" [{expectId}]{expectSummary}";
+
+            var (actualId, actualSummary) = ParseUtility.DecomposeHeading(input);
+
+            Assert.Equal(expectId, actualId);
+            Assert.Equal(expectSummary, actualSummary);
+        }
+    }
+}
