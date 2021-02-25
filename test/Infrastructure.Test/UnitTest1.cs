e7ac3871b1fd57f921b7b44849a07a1de8998839
miyoshi.yh <miyoshi.yh@om.asahi-kasei.co.jp>
2021-02-25 14:04:51 +0900

    Add test project

---
+++ test/Infrastructure.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace UsdmConverter.Infrastructure.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
