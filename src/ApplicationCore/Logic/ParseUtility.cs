f943987fff05fd6c1f2a7ec13cde5fd57f41342e
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-02-25 22:06:58 +0900

    Create ParseUtility.cs

---
+++ src/ApplicationCore/Logic/ParseUtility.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Toolkit.Parsers.Markdown.Blocks;
+
+namespace UsdmConverter.ApplicationCore.Logic
+{
+    static public class ParseUtility
+    {
+        static public (string id, string summary) DecomposeHeading(string header)
+        {
+            var text = header.Trim();
+            var prefix = Regex.Match(text, "\\[[-A-Z0-9]{1,}\\]").Value;
+            var id = prefix.Substring(1, prefix.Length - 2);
+            var summay = text?.Replace(prefix, string.Empty);
+
+            return string.IsNullOrEmpty(summay) ? (id, string.Empty) : (id, summay);
+        }
+
+    }
+}
