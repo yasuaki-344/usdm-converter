17addb445fedff41174a774989301fa40f39c680
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-02-27 15:08:32 +0900

    Update IExcelEncoder.cs

--- src/ApplicationCore/Interfaces/IExcelEncoder.cs
+++ src/ApplicationCore/Interfaces/IExcelEncoder.cs
@@ -2,10 +2,13 @@
 //
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
+using NPOI.SS.UserModel;
+using UsdmConverter.ApplicationCore.Entities;
 
 namespace UsdmConverter.ApplicationCore.Interfaces
 {
     public interface IExcelEncoder
     {
+        RequirementSpecification Encode(IWorkbook book);
     }
 }
