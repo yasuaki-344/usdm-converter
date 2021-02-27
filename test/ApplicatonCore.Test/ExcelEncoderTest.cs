2d0dcfc30914961195e46854fcfd2a3efa8bde0e
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-02-27 15:13:20 +0900

    Create ExcelEncoderTest.cs

---
+++ test/ApplicatonCore.Test/ExcelEncoderTest.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2021 Yasuaki Miyoshi
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+
+using System.Collections.Generic;
+using UsdmConverter.ApplicationCore.Entities;
+using UsdmConverter.ApplicationCore.Services;
+using Xunit;
+
+namespace UsdmConverter.ApplicatonCore.Test
+{
+    public class ExcelEncoderTest
+    {
+        [Fact]
+        public void EncodeCorrectly()
+        {
+            var target = new ExcelEncoder();
+        }
+    }
+}
