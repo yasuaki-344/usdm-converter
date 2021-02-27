9b9492b394168c31a3d1bd9e7fdc85474a53110d
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-02-27 14:32:53 +0900

    Update MarkdownComposer.cs

--- src/ApplicationCore/Services/MarkdownComposer.cs
+++ src/ApplicationCore/Services/MarkdownComposer.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 Yasuaki Miyoshi
+﻿// Copyright (c) 2021 Yasuaki Miyoshi
 //
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
