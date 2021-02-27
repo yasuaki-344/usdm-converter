aa80f4e62ca05ed2ac8d850f6e790939850e9f99
yasuaki-miyoshi <ikusay.443@gmail.com>
2021-02-27 14:32:30 +0900

    Create MarkdownComposerTest.cs

---
+++ test/ApplicatonCore.Test/MarkdownComposerTest.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2021 Yasuaki Miyoshi
+//
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+
+using UsdmConverter.ApplicationCore.Services;
+using Xunit;
+
+namespace UsdmConverter.ApplicatonCore.Test
+{
+    public class MarkdownComposerTest
+    {
+        [Fact]
+        public void ComposeCorrectly()
+        {
+            var target = new MarkdownComposer();
+        }
+    }
+}
